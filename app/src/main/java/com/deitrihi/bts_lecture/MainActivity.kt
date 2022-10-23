58c6ccddb7c3844d7f4f73015177dcecd688ebbf
deitrihi <deitrihi@naver.com>
2022-10-23 22:57:37 +0900

    123

---
+++ app/src/main/java/com/deitrihi/bts_lecture/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.deitrihi.bts_lecture
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 1. 화면이 클릭 되었다는 것을 알아야 합니다.
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌!
+            val intent = Intent(this, Bts1Activity::class.java)
+            startActivity(intent)
+        }
+    }
+}
